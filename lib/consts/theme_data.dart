222468909ce81159495acbc617e73239103d9d13
Basit ali <basitalyshah51214@gmail.com>
2023-11-18 08:52:51 -0800

    changes to add colors

--- lib/consts/theme_data.dart
+++ lib/consts/theme_data.dart
@@ -4,15 +4,16 @@ class Styles {
   static ThemeData themeData(bool isDarkTheme, BuildContext context) {
     return ThemeData(
       scaffoldBackgroundColor:
+          //0A1931  // white yellow 0xFFFCF8EC
           isDarkTheme ? const Color(0xFF00001a) : const Color(0xFFFFFFFF),
-      primaryColor: const Color(0xFFCB0166), // Change the primary color here
+      primaryColor: const Color(0xFFCB0166),
       colorScheme: ThemeData().colorScheme.copyWith(
             secondary:
-                isDarkTheme ? const Color(0xFFCB0166) : const Color(0xFFCB0166),
+                isDarkTheme ? const Color(0xFF1a1f3c) : const Color(0xFFE9FCFC),
             brightness: isDarkTheme ? Brightness.dark : Brightness.light,
           ),
       cardColor:
-          isDarkTheme ? const Color(0xFFEDBCD4) : const Color(0xFFEDBCD4),
+          isDarkTheme ? const Color(0xFF0a0d2c) : const Color(0xFFF2FDFD),
       canvasColor: isDarkTheme ? Colors.black : Colors.grey[50],
       buttonTheme: Theme.of(context).buttonTheme.copyWith(
           colorScheme: isDarkTheme
