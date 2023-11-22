21b8ed0431571d8ee8146c721ff46bf9c1967ef5
Basit ali <basitalyshah51214@gmail.com>
2023-11-22 08:12:15 -0800

    changes to add web image picker

--- lib/widgets/image_picker.dart
+++ lib/widgets/image_picker.dart
@@ -1,33 +1,33 @@
-import 'dart:io';
+// import 'dart:io';
 
-import 'package:flutter/foundation.dart';
-import 'package:image_picker/image_picker.dart';
+// import 'package:flutter/foundation.dart';
+// import 'package:image_picker/image_picker.dart';
 
-class ImagePick {
-  Future<void> pickImage(File productImage, Uint8List webImage) async {
-    if (!kIsWeb) {
-      final ImagePick picker = ImagePick();
-      XFile? image = await picker.pickImage(source: ImageSource.gallery);
-      if (image != null) {
-        var selected = File(image.path);
-        productImage = selected;
-      } else {
-        debugPrint("No Image Picked");
-      }
-    } else if (kIsWeb) {
-      final ImagePick picker = ImagePick();
+// class ImagePick {
+//   Future<void> pickImage(File productImage, Uint8List webImage) async {
+//     if (!kIsWeb) {
+//       final ImagePick picker = ImagePick();
+//       XFile? image = await picker.pickImage(source: ImageSource.gallery);
+//       if (image != null) {
+//         var selected = File(image.path);
+//         productImage = selected;
+//       } else {
+//         debugPrint("No Image Picked");
+//       }
+//     } else if (kIsWeb) {
+//       final ImagePick picker = ImagePick();
 
-      // Assume pickImage returns a Future<XFile?>
-      XFile? image = await picker.pickImage(source: ImageSource.gallery);
-      if (image != null) {
-        var fileBytes = await image.readAsBytes();
-        webImage = fileBytes;
-        productImage = File("a");
-      } else {
-        debugPrint("No Image Picked");
-      }
-    } else {
-      debugPrint("Something Went wrong");
-    }
-  }
-}
+//       // Assume pickImage returns a Future<XFile?>
+//       XFile? image = await picker.pickImage(source: ImageSource.gallery);
+//       if (image != null) {
+//         var fileBytes = await image.readAsBytes();
+//         webImage = fileBytes;
+//         productImage = File("a");
+//       } else {
+//         debugPrint("No Image Picked");
+//       }
+//     } else {
+//       debugPrint("Something Went wrong");
+//     }
+//   }
+// }
