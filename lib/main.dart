e87a6f2fa184b3fb13df62af92c867aef3c94cb3
Basit ali <basitalyshah51214@gmail.com>
2023-11-27 12:25:11 -0800

    changes to remove errors

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'screens/main_screen.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
