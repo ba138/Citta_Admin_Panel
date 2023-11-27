33abe7804ea36083d97d8d4a19a412a273e45cc6
Basit ali <basitalyshah51214@gmail.com>
2023-11-27 11:26:34 -0800

    changes to add loader

--- lib/inner_screen/add_product_screen.dart
+++ lib/inner_screen/add_product_screen.dart
@@ -3,6 +3,7 @@
 import 'dart:io';
 
 import 'package:citta_admin_panel/controllers/MenuController.dart';
+import 'package:citta_admin_panel/screens/loading.dart';
 import 'package:citta_admin_panel/services/global_method.dart';
 import 'package:citta_admin_panel/services/utils.dart';
 import 'package:citta_admin_panel/widgets/buttons.dart';
@@ -48,12 +49,12 @@ class _UploadProductFormState extends State<UploadProductForm> {
     super.dispose();
   }
 
-  bool _isLoading = false;
+  bool isLoading = false;
   void _uploadForm() async {
     final isValid = _formKey.currentState!.validate();
     FocusScope.of(context).unfocus();
     setState(() {
-      _isLoading = true;
+      isLoading = true;
     });
     if (isValid) {
       _formKey.currentState!.save();
@@ -82,16 +83,16 @@ class _UploadProductFormState extends State<UploadProductForm> {
       } on FirebaseException catch (error) {
         errorDialog(subtitle: '${error.message}', context: context);
         setState(() {
-          _isLoading = false;
+          isLoading = false;
         });
       } catch (error) {
         errorDialog(subtitle: '$error', context: context);
         setState(() {
-          _isLoading = false;
+          isLoading = false;
         });
       } finally {
         setState(() {
-          _isLoading = false;
+          isLoading = false;
         });
       }
     }
@@ -134,170 +135,175 @@ class _UploadProductFormState extends State<UploadProductForm> {
     return Scaffold(
       key: getAddProductscaffoldKey,
       drawer: const SideMenu(),
-      body: Row(
-        children: [
-          if (Responsive.isDesktop(context))
-            const Expanded(
-              child: SideMenu(),
-            ),
-          Expanded(
-            flex: 5,
-            child: SingleChildScrollView(
-              controller: ScrollController(),
-              child: Column(
-                children: [
-                  // Header(fct: () {
-                  //   controlAddProductsMenu();
-                  // }),
-                  Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: TextWidget(
-                      text: 'Add Product Details',
-                      color: color,
-                      isTitle: true,
-                    ),
-                  ),
-                  Container(
-                    width: size.width > 650 ? 650 : size.width,
-                    color: Theme.of(context).cardColor,
-                    padding: const EdgeInsets.all(16),
-                    margin: const EdgeInsets.all(16),
-                    child: Form(
-                      key: _formKey,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: <Widget>[
-                          TextWidget(
-                            text: 'Product title*',
-                            color: color,
-                            isTitle: true,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          TextFormField(
-                            controller: _titleController,
-                            key: const ValueKey('Title'),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Please enter a Title';
-                              }
-                              return null;
-                            },
-                            decoration: inputDecoration,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(14),
-                            child: Container(
-                              height:
-                                  size.width > 650 ? 350 : size.width * 0.45,
-                              color: Theme.of(context).scaffoldBackgroundColor,
-                              child: dottedBorder(
-                                color,
-                                pickImage,
-                                previewImage,
-                              ),
-                            ),
-                          ),
-                          TextWidget(
-                            text: 'Product Detail*',
-                            color: color,
-                            isTitle: true,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          TextField(
-                            maxLines: 4,
-                            decoration: InputDecoration(
-                              filled: true,
-                              fillColor: scaffoldColor,
-                              alignLabelWithHint: true,
-                              hintStyle: TextStyle(
-                                fontSize: 14,
-                                fontWeight: FontWeight.w400,
-                                color: color,
-                              ),
-                              hintText: 'Write details about Product....',
-                              border: InputBorder.none,
-                              focusedBorder: OutlineInputBorder(
-                                borderSide: BorderSide(
-                                  color: color,
-                                  width: 1.0,
-                                ),
-                              ),
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 20,
-                          ),
-                          TextWidget(
-                            text: 'Product Price*',
-                            color: color,
-                            isTitle: true,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          TextFormField(
-                            controller: _priceController,
-                            key: const ValueKey('Price'),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Please enter a Price';
-                              }
-                              return null;
-                            },
-                            decoration: inputDecoration,
-                          ),
-                          TextWidget(
-                            text: 'Product Amount*',
-                            color: color,
-                            isTitle: true,
-                          ),
-                          const SizedBox(
-                            height: 10,
-                          ),
-                          TextFormField(
-                            controller: _amountController,
-                            key: const ValueKey('Amount'),
-                            validator: (value) {
-                              if (value!.isEmpty) {
-                                return 'Please enter a Amount';
-                              }
-                              return null;
-                            },
-                            decoration: inputDecoration,
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(18.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceAround,
-                              children: [
-                                ButtonsWidget(
-                                  onPressed: () {},
-                                  text: 'Clear form',
-                                ),
-                                ButtonsWidget(
-                                  onPressed: () {
-                                    _uploadForm();
-                                  },
-                                  text: 'Upload',
-                                ),
-                              ],
-                            ),
-                          )
-                        ],
+      body: LoadingManager(
+        isLoading: isLoading,
+        child: Row(
+          children: [
+            if (Responsive.isDesktop(context))
+              const Expanded(
+                child: SideMenu(),
+              ),
+            Expanded(
+              flex: 5,
+              child: SingleChildScrollView(
+                controller: ScrollController(),
+                child: Column(
+                  children: [
+                    // Header(fct: () {
+                    //   controlAddProductsMenu();
+                    // }),
+                    Padding(
+                      padding: const EdgeInsets.all(20),
+                      child: TextWidget(
+                        text: 'Add Product Details',
+                        color: color,
+                        isTitle: true,
                       ),
                     ),
-                  ),
-                ],
+                    Container(
+                      width: size.width > 650 ? 650 : size.width,
+                      color: Theme.of(context).cardColor,
+                      padding: const EdgeInsets.all(16),
+                      margin: const EdgeInsets.all(16),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            TextWidget(
+                              text: 'Product title*',
+                              color: color,
+                              isTitle: true,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              controller: _titleController,
+                              key: const ValueKey('Title'),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter a Title';
+                                }
+                                return null;
+                              },
+                              decoration: inputDecoration,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(14),
+                              child: Container(
+                                height:
+                                    size.width > 650 ? 350 : size.width * 0.45,
+                                color:
+                                    Theme.of(context).scaffoldBackgroundColor,
+                                child: dottedBorder(
+                                  color,
+                                  pickImage,
+                                  previewImage,
+                                ),
+                              ),
+                            ),
+                            TextWidget(
+                              text: 'Product Detail*',
+                              color: color,
+                              isTitle: true,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextField(
+                              maxLines: 4,
+                              decoration: InputDecoration(
+                                filled: true,
+                                fillColor: scaffoldColor,
+                                alignLabelWithHint: true,
+                                hintStyle: TextStyle(
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                  color: color,
+                                ),
+                                hintText: 'Write details about Product....',
+                                border: InputBorder.none,
+                                focusedBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                    color: color,
+                                    width: 1.0,
+                                  ),
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 20,
+                            ),
+                            TextWidget(
+                              text: 'Product Price*',
+                              color: color,
+                              isTitle: true,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              controller: _priceController,
+                              key: const ValueKey('Price'),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter a Price';
+                                }
+                                return null;
+                              },
+                              decoration: inputDecoration,
+                            ),
+                            TextWidget(
+                              text: 'Product Amount*',
+                              color: color,
+                              isTitle: true,
+                            ),
+                            const SizedBox(
+                              height: 10,
+                            ),
+                            TextFormField(
+                              controller: _amountController,
+                              key: const ValueKey('Amount'),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter a Amount';
+                                }
+                                return null;
+                              },
+                              decoration: inputDecoration,
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(18.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  ButtonsWidget(
+                                    onPressed: () {},
+                                    text: 'Clear form',
+                                  ),
+                                  ButtonsWidget(
+                                    onPressed: () {
+                                      _uploadForm();
+                                    },
+                                    text: 'Upload',
+                                  ),
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
