c5034454a9ecea02e788352cf3692dfaef0347a7
Basit ali <basitalyshah51214@gmail.com>
2023-12-04 02:45:38 -0800

    changes to get the image from gellry

--- lib/widgets/image_picker.dart
+++ lib/widgets/image_picker.dart
@@ -1,20 +1,43 @@
-import 'dart:html' as html;
-import 'dart:typed_data';
+// import 'dart:html' as html;
+// import 'dart:typed_data';
+
+// import 'package:flutter/material.dart';
+// import 'package:image_picker/image_picker.dart';
+
+// class ImagePick {
+//   static Future<void> pickImage(
+//       ImageSource source, html.File? imageFile, Image? previewImage) async {
+//     final pickedFile = await ImagePicker().pickImage(source: source);
+
+//     if (pickedFile != null) {
+//       final bytes = await pickedFile.readAsBytes();
+//       final image = Image.memory(Uint8List.fromList(bytes));
+
+//       imageFile = pickedFile as html.File?;
+//       previewImage = image;
+//     }
+//   }
+// }
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:image_picker/image_picker.dart';
 
-class ImagePick {
-  static Future<void> pickImage(
-      ImageSource source, html.File? imageFile, Image? previewImage) async {
-    final pickedFile = await ImagePicker().pickImage(source: source);
-
-    if (pickedFile != null) {
-      final bytes = await pickedFile.readAsBytes();
-      final image = Image.memory(Uint8List.fromList(bytes));
-
-      imageFile = pickedFile as html.File?;
-      previewImage = image;
+Future<File?> pickImageFromGallery(
+  BuildContext context,
+) async {
+  File? image;
+  try {
+    final pickedImage =
+        await ImagePicker().pickImage(source: ImageSource.gallery);
+    if (pickedImage != null) {
+      image = File(
+        pickedImage.path,
+      );
     }
+  } catch (e) {
+    Fluttertoast.showToast(msg: e.toString());
   }
+  return image;
 }
