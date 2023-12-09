829a204ff6813f459a4c3ba02615ecbf90f3553d
Basit ali <basitalyshah51214@gmail.com>
2023-12-09 01:41:42 -0800

    changes in register screen

--- lib/auth/screens/register_screen.dart
+++ lib/auth/screens/register_screen.dart
@@ -1,10 +1,18 @@
+// ignore_for_file: unused_element, no_leading_underscores_for_local_identifiers
+
+import 'dart:io';
+
 import 'package:citta_admin_panel/auth/screens/login_screen.dart';
 import 'package:citta_admin_panel/responsive.dart';
 import 'package:citta_admin_panel/screens/loading.dart';
 import 'package:citta_admin_panel/screens/main_screen.dart';
 import 'package:citta_admin_panel/services/utils.dart';
+import 'package:citta_admin_panel/widgets/buttons.dart';
 import 'package:citta_admin_panel/widgets/text_widget.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -19,6 +27,38 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
   bool isLoading = false;
+  File? _pickedImage;
+  Uint8List webImage = Uint8List(8);
+  Future<void> _pickImage() async {
+    if (!kIsWeb) {
+      final ImagePicker _picker = ImagePicker();
+      XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        var selected = File(image.path);
+        setState(() {
+          _pickedImage = selected;
+        });
+      } else {
+        Fluttertoast.showToast(msg: "No Image has been Picked");
+      }
+    } else if (kIsWeb) {
+      final ImagePicker _picker = ImagePicker();
+      XFile? image = await _picker.pickImage(source: ImageSource.gallery);
+      if (image != null) {
+        var f = await image.readAsBytes();
+
+        setState(() {
+          webImage = f;
+          _pickedImage = File("a");
+        });
+      } else {
+        Fluttertoast.showToast(msg: "No Image has been Picked");
+      }
+    } else {
+      Fluttertoast.showToast(msg: "Something went wrong");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final color = Utils(context).color;
@@ -82,6 +122,15 @@ class _RegisterScreenState extends State<RegisterScreen> {
                             mainAxisAlignment: MainAxisAlignment.start,
                             mainAxisSize: MainAxisSize.min,
                             children: <Widget>[
+                              const Center(
+                                child: CircleAvatar(
+                                  radius: 60,
+                                ),
+                              ),
+                              ButtonsWidget(
+                                onPressed: () {},
+                                text: "Select Image",
+                              ),
                               TextWidget(
                                 text: 'Email*',
                                 color: color,
