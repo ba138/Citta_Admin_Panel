9a52a801adf5c04d333f1947f07f974a5d458965
Basit ali <basitalyshah51214@gmail.com>
2023-12-09 10:20:33 -0800

    adding splash screen to the app

--- lib/auth/screens/splash_screen.dart
+++ lib/auth/screens/splash_screen.dart
@@ -1,8 +1,5 @@
-import 'package:citta_admin_panel/auth/screens/register_screen.dart';
 import 'package:citta_admin_panel/responsive.dart';
-import 'package:citta_admin_panel/screens/loading.dart';
 import 'package:citta_admin_panel/services/utils.dart';
-import 'package:citta_admin_panel/widgets/text_widget.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatefulWidget {
@@ -19,148 +16,37 @@ class _SplashScreenState extends State<SplashScreen> {
 
     return Scaffold(
       body: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           if (Responsive.isDesktop(context))
             Expanded(
               flex: 8,
-              child: SingleChildScrollView(
-                controller: ScrollController(),
-                child: Column(
-                  children: [
-                    Padding(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: Padding(
                       padding: const EdgeInsets.all(20),
                       child: Column(
                         children: [
                           Container(
-                            height: 100,
-                            width: 100,
+                            width: size.width > 650 ? 650 : size.width,
+                            padding: const EdgeInsets.all(16),
                             decoration: const BoxDecoration(
+                              color: Colors.transparent,
                               image: DecorationImage(
                                   image:
                                       AssetImage("assets/images/groceries.png"),
                                   fit: BoxFit.contain),
                             ),
-                          ),
-                          TextWidget(
-                            text: 'Saller Point',
-                            color: Colors.black54,
-                            isTitle: true,
-                          ),
+                          )
                         ],
                       ),
                     ),
-                    Container(
-                      width: size.width > 650 ? 650 : size.width,
-                      color: Theme.of(context).cardColor,
-                      padding: const EdgeInsets.all(16),
-                      margin: const EdgeInsets.all(16),
-                      child: Form(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.min,
-                          children: <Widget>[
-                            TextWidget(
-                              text: 'Email*',
-                              color: Colors.black54,
-                              isTitle: true,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              key: const ValueKey('Email'),
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return 'Please enter user Email';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            TextWidget(
-                              text: 'Password*',
-                              color: Colors.black54,
-                              isTitle: true,
-                            ),
-                            const SizedBox(
-                              height: 10,
-                            ),
-                            TextFormField(
-                              key: const ValueKey('Password'),
-                              validator: (value) {
-                                if (value!.isEmpty) {
-                                  return 'Please enter your Password';
-                                }
-                                return null;
-                              },
-                            ),
-                            const SizedBox(
-                              height: 20,
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: Center(
-                                child: InkWell(
-                                  onTap: () async {
-                                    // signinUser();
-                                  },
-                                  child: Container(
-                                    height: 46,
-                                    width: 300,
-                                    color: const Color(0xFFCB0166),
-                                    child: const Center(
-                                      child: Text(
-                                        "Login",
-                                        style: TextStyle(
-                                          fontSize: 18,
-                                          color: Colors.white,
-                                        ),
-                                      ),
-                                    ),
-                                  ),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: const EdgeInsets.all(18.0),
-                              child: Row(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                children: [
-                                  const Text(
-                                    "Don't have account?",
-                                  ),
-                                  const SizedBox(
-                                    width: 6,
-                                  ),
-                                  InkWell(
-                                    onTap: () {
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (c) =>
-                                              const RegisterScreen(),
-                                        ),
-                                      );
-                                    },
-                                    child: const Text(
-                                      "Signup",
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  )
-                                ],
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
+                  ),
+                ],
               ),
             ),
         ],
