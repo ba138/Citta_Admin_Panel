8b6f4725ccada3aa8e0fbcd3ea84818d36e2264f
Basit ali <basitalyshah51214@gmail.com>
2023-12-12 09:46:05 -0800

    changes to ingnore the build context in async

--- lib/widgets/fashion_widget.dart
+++ lib/widgets/fashion_widget.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: library_private_types_in_public_api
+// ignore_for_file: library_private_types_in_public_api, use_build_context_synchronously
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:fancy_shimmer_image/fancy_shimmer_image.dart';
