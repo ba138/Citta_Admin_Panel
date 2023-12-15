363c69111df9bcd2033d75b74af68fa5857b9cbc
Basit ali <basitalyshah51214@gmail.com>
2023-12-15 10:05:47 -0800

    changes in edit screen

--- lib/inner_screen/edit_product_screen.dart
+++ lib/inner_screen/edit_product_screen.dart
@@ -274,7 +274,7 @@ class _EditProductScreenState extends State<EditProductScreen> {
                     Padding(
                       padding: const EdgeInsets.all(20),
                       child: TextWidget(
-                        text: 'Add Product Details',
+                        text: 'Edit Product Details',
                         color: color,
                         isTitle: true,
                       ),
