188e570c919929572f50699476048b26de584091
Basit ali <basitalyshah51214@gmail.com>
2024-01-13 01:29:13 -0800

    changes to add finance data to the screen

--- lib/inner_screen/finance_screen.dart
+++ lib/inner_screen/finance_screen.dart
@@ -1,5 +1,6 @@
 import 'package:citta_admin_panel/consts/constants.dart';
 import 'package:citta_admin_panel/controllers/MenuController.dart';
+import 'package:citta_admin_panel/models.dart';
 import 'package:citta_admin_panel/responsive.dart';
 import 'package:citta_admin_panel/services/utils.dart';
 import 'package:citta_admin_panel/widgets/header.dart';
@@ -10,6 +11,7 @@ import 'package:citta_admin_panel/widgets/web_container.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class FinanceScreen extends StatefulWidget {
   const FinanceScreen({super.key});
@@ -19,56 +21,104 @@ class FinanceScreen extends StatefulWidget {
 }
 
 class _FinanceScreenState extends State<FinanceScreen> {
-  String totalOrders = "0";
-  String complete = "0";
+  String totalIncome = "0";
+  String currentMonthIcome = "0";
 
-  String pending = "0";
+  String currentDayicome = "0";
 
-  String processing = "0";
+  String pevriousMonthIcome = "0";
 
-  Future<void> analyzeOrders() async {
-    try {
-      QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-          .collection("saller")
-          .doc(FirebaseAuth.instance.currentUser!.uid)
-          .collection("my_orders")
-          .get();
+  Future<double> calculateSum(Query query) async {
+    double totalSalePrice = 0.0;
 
-      int collectionIndex = 0;
+    QuerySnapshot querySnapshot = await query.get();
+    List<DocumentSnapshot> documents = querySnapshot.docs;
 
-      int pendingCount = 0;
-      int processingCount = 0;
-      int completeCount = 0;
-
-      for (QueryDocumentSnapshot doc in querySnapshot.docs) {
-        String status = doc['status'] ?? '';
-
-        if (status == 'pending') {
-          pendingCount++;
-        } else if (status == 'processing') {
-          processingCount++;
-        } else if (status == 'Delivered') {
-          completeCount++;
-        }
-
-        collectionIndex++;
-      }
-
-      setState(() {
-        totalOrders = collectionIndex.toString();
-        pending = pendingCount.toString();
-        processing = processingCount.toString();
-        complete = completeCount.toString();
-      });
-    } catch (e) {
-      debugPrint('Error analyzing orders: $e');
+    for (DocumentSnapshot document in documents) {
+      double salePrice = double.parse(document['salePrice'] ?? '0.0');
+      totalSalePrice += salePrice;
     }
+
+    return totalSalePrice;
+  }
+
+  String formatDateAndTime(String dateTime) {
+    // Assuming the date format in Firestore is 'yyyy-MM-dd HH:mm:ss.SSSSSS'
+    DateTime parsedDate = DateTime.parse(dateTime);
+
+    // Format it as needed, for example: 'yyyy-MM-dd HH:mm:ss.SSSSSS'
+    String formattedDate =
+        DateFormat('yyyy-MM-dd HH:mm:ss.SSSSSS').format(parsedDate);
+
+    return formattedDate;
   }
 
   @override
   void initState() {
     super.initState();
-    analyzeOrders();
+    fetchData();
+    sumAndAssignSalePrices();
+  }
+
+  void fetchData() async {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    CollectionReference collectionReference = FirebaseFirestore.instance
+        .collection("saller")
+        .doc(uid)
+        .collection("my_orders");
+
+    DateTime currentDate = DateTime.now();
+    String todayFormatted = DateFormat('yyyy-MM-dd').format(currentDate);
+
+    // Calculate the first day of the current month
+    DateTime firstDayOfCurrentMonth =
+        DateTime(currentDate.year, currentDate.month, 1);
+
+    // Calculate the first day of the previous month
+    DateTime firstDayOfPreviousMonth =
+        firstDayOfCurrentMonth.subtract(const Duration(days: 1));
+
+    Query todayQuery = collectionReference
+        .where('date', isGreaterThanOrEqualTo: '$todayFormatted 00:00:00')
+        .where('date', isLessThanOrEqualTo: '$todayFormatted 23:59:59');
+
+    Query currentMonthQuery = collectionReference.where('date',
+        isGreaterThanOrEqualTo: firstDayOfCurrentMonth.toString());
+
+    Query previousMonthQuery = collectionReference
+        .where('date', isGreaterThanOrEqualTo: firstDayOfPreviousMonth.toUtc())
+        .where('date', isLessThan: firstDayOfCurrentMonth.toUtc());
+    double todayTotalSalePrice = await calculateSum(todayQuery);
+
+    double currentMonthTotalSalePrice = await calculateSum(currentMonthQuery);
+
+    double previousMonthTotalSalePrice = await calculateSum(previousMonthQuery);
+    debugPrint(
+        'Total sale price for the previous month: $previousMonthTotalSalePrice');
+
+    setState(() {
+      currentMonthIcome = currentMonthTotalSalePrice.toString();
+      pevriousMonthIcome = previousMonthTotalSalePrice.toString();
+      currentDayicome = todayTotalSalePrice.toString();
+    });
+  }
+
+  Future<void> sumAndAssignSalePrices() async {
+    QuerySnapshot querySnapshot = await FirebaseFirestore.instance
+        .collection("saller")
+        .doc(FirebaseAuth.instance.currentUser!.uid)
+        .collection("my_orders")
+        .get();
+
+    int totalSalePrice = 0;
+
+    for (QueryDocumentSnapshot doc in querySnapshot.docs) {
+      int salePrice = int.tryParse(doc['salePrice'] ?? '0') ?? 0;
+
+      totalSalePrice += salePrice;
+    }
+
+    totalIncome = totalSalePrice.toString();
   }
 
   @override
@@ -107,7 +157,7 @@ class _FinanceScreenState extends State<FinanceScreen> {
                         mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           TextWidget(
-                            text: 'All Details',
+                            text: 'All Finance Details',
                             color: color,
                             textSize: 24,
                             isTitle: true,
@@ -124,13 +174,13 @@ class _FinanceScreenState extends State<FinanceScreen> {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 MobileContainer(
-                                  title: "Total Orders",
-                                  number: complete,
+                                  title: "Total Income",
+                                  number: "₹$totalIncome",
                                   color: Colors.white,
                                 ),
                                 MobileContainer(
-                                  title: "Pending",
-                                  number: pending,
+                                  title: "Current Month",
+                                  number: "₹$currentMonthIcome",
                                   color: Colors.white,
                                 ),
                               ],
@@ -142,13 +192,13 @@ class _FinanceScreenState extends State<FinanceScreen> {
                               mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 MobileContainer(
-                                  title: processing,
-                                  number: "4",
+                                  title: "Current Day",
+                                  number: "₹$currentDayicome",
                                   color: Colors.white,
                                 ),
                                 MobileContainer(
-                                  title: "Complete",
-                                  number: complete,
+                                  title: "Previous Month",
+                                  number: "₹$pevriousMonthIcome",
                                   color: Colors.white,
                                 ),
                               ],
@@ -159,20 +209,20 @@ class _FinanceScreenState extends State<FinanceScreen> {
                           mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             WebContainer(
-                              number: totalOrders,
-                              title: "Total Orders",
+                              number: "₹$totalIncome",
+                              title: "Total Icome",
                             ),
                             WebContainer(
-                              number: pending,
-                              title: "Pending",
+                              number: "₹$currentMonthIcome",
+                              title: "Current Month",
                             ),
                             WebContainer(
-                              number: processing,
-                              title: "Processing",
+                              number: "₹$currentDayicome",
+                              title: "Current Day",
                             ),
                             WebContainer(
-                              number: complete,
-                              title: "Complete",
+                              number: "₹$pevriousMonthIcome",
+                              title: "Previous Month",
                             ),
                           ],
                         ),
---
+++ lib/models.dart
@@ -0,0 +1,6 @@
+class Finance {
+  final String date;
+  final double salePrice;
+
+  Finance({required this.date, required this.salePrice});
+}
